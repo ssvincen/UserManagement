35c12bdabf8cec513a2ff78d57c88c07bf60ff47
Sifiso Sikhakhane <ss.vincen@gmail.com>
2021-09-23 15:02:33 +0200

    Upda

--- UserManagement.Database/StoreProcedures/Account/spr_GetPermissionByGroupId.sql
+++ UserManagement.Database/StoreProcedures/Account/spr_GetPermissionByGroupId.sql
@@ -6,17 +6,18 @@ BEGIN
 	SET NOCOUNT ON;
 	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 
-	  SELECT nm.Id
-			,nm.[Name]
-			,nm.Area
-			,nm.ActionName
-			,nm.ControllerName
-			,nm.IsExternal
-			,nm.ExternalUrl
-			,nm.DisplayOrder
-			,nm.ParentMenuId
-			,nm.Visible
-	  FROM dbo.NavigationMenu nm LEFT JOIN dbo.GroupMenuPermission gmp
-	  ON nm.Id = gmp.NavigationMenuId
-	  WHERE gmp.GroupId = @GroupId
+	SELECT nm.Id,
+		   nm.[Name],
+		   nm.ParentMenuId,
+		   nm.Area,
+		   nm.ControllerName,
+		   nm.ActionName,
+		   nm.IsExternal,
+		   nm.IsExternal,
+		   nm.DisplayOrder,
+		   nm.Visible,
+		   CASE WHEN gmp.GroupId IS NULL THEN 0 ELSE 1 END 'Permitted'
+	FROM dbo.NavigationMenu nm LEFT JOIN dbo.GroupMenuPermission gmp 
+	ON nm.Id = gmp.NavigationMenuId 
+	AND gmp.GroupId = @GroupId
 END
